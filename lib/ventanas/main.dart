e70481847bd324a8382940e219054b615768ac20
Gabrigol17 <gabrielangelarrieta3@gmail.com>
2025-05-19 07:56:55 -0500

    limpiando espacio

--- lib/ventanas/main.dart
+++ lib/ventanas/main.dart
@@ -21,8 +21,8 @@ class MyApp extends StatelessWidget {
             ],
           ),
         ),
-      ),);
+      ),
+      );
       
   }
-
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:arcane_characters/main.dart';
+import 'package:arcane_characters/ventanas/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
